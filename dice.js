aef169d803e19d15575a6ef185745b6fe849dd40
Lily Barrett <11barrettpl@gmail.com>
2023-07-23 07:36:16 +1200

    Addition of js file. Process of completing the diceRoll function

---
+++ dice.js
@@ -0,0 +1,7 @@
+function rollDice() {
+    const dice = document.getElementById("dice");
+    const result = document.getElementById("result");
+    const diceSidesSelect = document.getElementById("diceSides");
+
+    dice.style.animation = "rollAnimation 1s ease-in-out";
+}
