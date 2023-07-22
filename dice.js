47bf653336ce39d8875edfa60aad681d51bd621e
Lily Barrett <11barrettpl@gmail.com>
2023-07-23 07:47:33 +1200

    Finished the first version of my js file. Functions and animations completed.

--- dice.js
+++ dice.js
@@ -1,6 +1,6 @@
 function rollDice() {
     const dice = document.getElementById("dice");
-    const result = document.getElementById("result");
+    const resultE = document.getElementById("result");
     const diceSidesSelect = document.getElementById("diceSides");
 
     dice.style.animation = "rollAnimation 1s ease-in-out";
@@ -8,6 +8,22 @@ function rollDice() {
     setTimeout (() => {
         const selectedDiceSides = parseInt(diceSidesSelect.value);
         const number = Math.floor(Math.random() * selectedDiceSides) + 1;
-        
-    })
+
+        dice.innerHTML = `<img src="dice.png" alt="Dice Image" id="rollButton">`;
+        resultE.textcontent = number;
+
+        setTimeout(() => {
+            dice.style.animation = "";
+        }, 100);
+
+        setTimeout(() => {
+            resultE.style.display = "none";
+        }, 3000);
+    }, 1000);
 }
+
+document.body.addEventListener("click", function(event) {
+    if (event.target.id === "rollButton") {
+        rollDice();
+    }
+});
